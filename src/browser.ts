f85556da70f25cf37f7193abd46280fd8f109880
aki <lala.akira@gmail.com>
2020-07-04 16:24:07 +0900

    feat: Support vmax and vmin

--- src/browser.ts
+++ src/browser.ts
@@ -3,7 +3,17 @@ import debounceFn from 'debounce-fn';
 // SEE: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 const setCssProperty = () => {
   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  const vw = window.innerWidth * 0.01;
+  const root = document.documentElement;
+
+  root.style.setProperty('--vh', `${vh}px`);
+  if (vh > vw) {
+    root.style.setProperty('--vmax', `${vh}px`);
+    root.style.setProperty('--vmin', `${vw}px`);
+  } else {
+    root.style.setProperty('--vmax', `${vw}px`);
+    root.style.setProperty('--vmin', `${vh}px`);
+  }
 };
 
 export function viewportUnitOnMobile() {
